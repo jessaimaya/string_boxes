4e4712e0f153c9ebda9a03a3aa3fac7dcd8607d9
jessai <maya.jessai@gmail.com>
2021-11-11 16:40:01 -0600

    feat: design prop 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate nannou;
 use to_binary::{BinaryString};
-use nannou::{prelude::*, rand::Rng};
+use nannou::{prelude::*, rand::Rng, ui::color::TRANSPARENT};
 use rand_seeder::Seeder;
 use rand_pcg::Pcg64;
 
@@ -16,7 +16,9 @@ struct Cell {
     size: f32,
     pos: (f32, f32),
     cells: Option<Vec<Cell>>,
+    col: bool,
     letter: Option<char>,
+    fill : bool,
 }
 
 #[derive(Debug, Clone)]
@@ -28,10 +30,10 @@ struct Model {
 }
 
 fn model(_app: &App) -> Model {
-    let size = 25.0;
+    let size = 60.0;
     let gutter = 4.0;
-    //let seed = "pwoornaebtgeml".to_string();
-    let seed = "abcdefghijklmnopqrstuvwxyz".to_string();
+    let seed = "empowerglobant".to_string();
+    //let seed = "abcdefghijklmnopqrstuvwxyz".to_string();
     let grid = gen_grid(size, gutter, &seed);
 
     Model {
@@ -57,7 +59,9 @@ fn view(app: &App, model: &Model, frame: Frame){
 }
 
 fn draw_boxes(draw: &mut Draw, model: &Model) {
-
+    let letters: Vec<char> = model.seed.chars().collect();
+    let letters_half = letters.len() / 4;
+    
     for parent in model.grid.iter() {
         let cha = parent.letter.unwrap();
         let hex = format!("{:x}", cha as i32);
@@ -67,30 +71,68 @@ fn draw_boxes(draw: &mut Draw, model: &Model) {
                                 .chars()
                                 .map(|b| !(b == '0'))
                                 .collect();
-        //println!("c: {:?}, h: {:?}, b:{:?}", cha, hex, bits);
 
         if let Some(cells) = &parent.cells {
-            for (ind, cell) in cells.iter().enumerate() {
-                cells_iter(cell, draw, bits[ind + 1]);
+            for (ind,cell) in cells.iter().enumerate() {
+                cells_iter(cell, draw, &bits, ind, letters_half);
             }
         }
     }
 
-    fn cells_iter(cell: &Cell, draw: &mut Draw, on:bool) {
+    fn cells_iter(cell: &Cell, draw: &mut Draw, indx: &Vec<bool>, curr: usize, half: usize) {
         if let Some(cell) = &cell.cells {
-            for cel in cell.iter() {
-                cells_iter(cel, draw, on);
+            for (ind, cel) in cell.iter().enumerate() {
+                cells_iter(cel, draw, indx, curr + ind + 1, half);
             }
         } else {
-            
-            let c = if on { YELLOWGREEN } else { BLUEVIOLET};
 
-            draw.rect()
-                .x_y(cell.pos.0, cell.pos.1)
-                .w_h(cell.size, cell.size)
-                .stroke(WHITE)
-                .stroke_weight(1.0)
-                .color(c);
+            if indx[curr] {
+                let c = if cell.col {
+                    // Green Globant
+                    Rgb::new(194u8, 214u8, 71u8)
+                } else {
+                    // Blue Empower
+                    Rgb::new(0u8, 65u8, 133u8)
+                    //Rgb::new(219u8, 24u8, 48u8)
+                };
+
+
+            if cell.col {
+                if cell.fill {
+                     draw.ellipse()
+                    .x_y(cell.pos.0, cell.pos.1)
+                    .w_h(cell.size, cell.size)
+                    .stroke(BLACK)
+                    .stroke_weight(2.0)
+                    .color(c);
+
+                } else {
+                    draw.ellipse()
+                    .x_y(cell.pos.0, cell.pos.1)
+                    .w_h(cell.size, cell.size)
+                    .no_fill()
+                    .stroke(c)
+                    .stroke_weight(2.0);
+                }
+
+            } else {
+                if cell.fill {
+                    draw.rect()
+                    .x_y(cell.pos.0, cell.pos.1)
+                    .w_h(cell.size, cell.size)
+                    .stroke(BLACK)
+                    .stroke_weight(1.0)
+                    .color(c);
+                } else {
+                    draw.rect()
+                    .x_y(cell.pos.0, cell.pos.1)
+                    .w_h(cell.size, cell.size)
+                    .no_fill()
+                    .stroke(c)
+                    .stroke_weight(1.0);
+                }
+            }
+            }
 
         }
     }
@@ -112,17 +154,17 @@ fn gen_grid(size: f32, gutter: f32, seed: &str) -> Vec<Cell> {
                 size,
                 pos: coords,
                 cells: None,
+                col: i % 2 == 0,
+                fill: rng.gen_bool(0.5),
                 letter: Some(chars[(i + j as u32) as usize]),
             };
 
             parent.gen_cells();
+
             let p_cells = parent.cells.as_mut().unwrap();
             let r = rng.gen_range(0..4);
 
             p_cells[r.clone() as usize].gen_cells();
-
-            
-           //  println!("Parent: {:#?}", parent);
 
             base_grid.push(parent);
         }
@@ -131,10 +173,15 @@ fn gen_grid(size: f32, gutter: f32, seed: &str) -> Vec<Cell> {
 
     fn get_coors(col: u32, row: u32, size: f32, gutter: f32) -> (f32, f32) {
         let center = size;
-        let pos_x = (center * (col + 1) as f32) + gutter * (col + 1) as f32;
+        let c = (col + 1) as f32;
+        let r = (row + 1) as f32;
+
+        let pos_x = (gutter * c) + (size * c);
         let pos_y = (gutter * (row + 1) as f32) + (center * (row + 1) as f32);
 
-        (pos_x, pos_y)
+        // TODO: dynamic centering with len of rows and cols
+        (pos_x - (size * 2.0), (pos_y - (size * 7.5)))
+
     }
      
 
@@ -146,13 +193,16 @@ impl Cell {
         self.cells = self.gen_boxes();
     }
 
-    // fn get_boxes (&mut self, px: f32, py:f32, size: f32) -> Vec<Cell> {
     pub fn gen_boxes (&mut self) -> Option<Vec<Cell>> {
+        let mut rng: Pcg64 = Seeder::from(self.letter).make_rng();
+
         let half = self.size / 4.0;
         let base = Cell {
-            size: self.size / 2.0,
+            size: (self.size / 2.0) - 2.0,
             pos: (0.0, 0.0),
             cells: None,
+            col: self.col,
+            fill: rng.gen_bool(0.5),
             letter: None,
         };
         let px = self.pos.0;
